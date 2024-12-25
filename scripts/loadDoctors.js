b0d50e88450050b9b0906bb6f653fe6af0e96b14
kevinjankowski <kevin.jankowski0@gmail.com>
2024-12-25 14:35:48 +0100

    dokończenie funckji loadDoctors.js

--- scripts/loadDoctors.js
+++ scripts/loadDoctors.js
@@ -4,8 +4,32 @@ import { apiQuery } from './apiQuery.js';
 // Zmienne
 const submitButton = document.getElementById("submit");
 const specializationList = document.getElementById("specializations-list");
+const listOfDoctors  = document.getElementById("list-of-doctors");
+const  listText  = document.getElementById("list-text");
 
 // Funkcje
+
+/* Funkcj doctorElement tworzy kafelek z imieniem, nazwiskiem oraz specjalizacjami danego lekarza */
+function doctorElement(doctor) {
+    const doctorDiv = document.createElement("div");
+    doctorDiv.id = "doctor-div";
+
+    // Stworzenie nagłówka imienia i nazwiska
+    const doctorName = document.createElement("h4");
+    doctorName.textContent = `${doctor.first_name} ${doctor.last_name}`;
+
+    // Stworzenie paragrafu z wszystkimi specjalizacjiami
+    const doctorSpecializations = document.createElement("p");
+    doctorSpecializations.textContent = doctor.specializations.join(", ");
+
+    // Dodanie danych do kafelka
+    doctorDiv.appendChild(doctorName);
+    doctorDiv.appendChild(doctorSpecializations);
+
+    // Dodanie kafelka do kontenera list-of-doctors
+    listOfDoctors.appendChild(doctorDiv);
+}
+
 /*  Poniższa funkcja wywołuje zaimportowaną funkcję apiQuery, która pobiera z bazy danych wszystkich lekarzy
 *   o podanej w parametrze specjalizacji. Specjalizacja pobrana jest z listy rozwijanej typu <select> */
 function loadDoctors(selectedSpecialization) {
@@ -16,7 +40,13 @@ function loadDoctors(selectedSpecialization) {
 
     apiQuery(endpoint, params)
         .then((data) => {
-            console.log(data);
+            data.forEach((item) => {
+                // Wywołanie funckji, która dodaje kafelek z danymi lekarza podanego w patametrze
+                doctorElement(item)
+            })
+        })
+        .catch((error) => {
+            console.error("Błąd podczas pobierania lekarzy:", error.message);
         })
 }
 
@@ -25,6 +55,14 @@ function loadDoctors(selectedSpecialization) {
 /*  Poniższy fragment dodaje addEventListener do przycisku "Wybierz".
 *   Po naciśnięciu guzika wywoływana jest funkcja loadDoctors */
 submitButton.addEventListener("click", function () {
+
+    // Odkrycie napisu listText
+    listText.style.display = "block";
+
+    // Wyczyszczenie obecnej listy lekarzy
+    listOfDoctors.innerHTML = "";
+
+    // Pobranie do zmiennej nazwy specjalizacji z listy typu <select> oraz wywołanie funkcji loadDoctors
     const selectedSpecialization = specializationList.value;
     loadDoctors(selectedSpecialization)
 })
